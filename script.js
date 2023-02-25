c4035f7690262fa70c4fc573be1a90c2a6de5abe
erick fonseca <32956392+erickpiazza@users.noreply.github.com>
2023-02-25 18:37:46 -0300

    feat: adicionando animacoes e trocando imagens

--- script.js
+++ script.js
@@ -12,6 +12,6 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/avatar-light.png")
   } else {
-    img.setAttribute("src", "./assets/avatar-dark.jpeg")
+    img.setAttribute("src", "./assets/avatar-dark.png")
   }
 }
